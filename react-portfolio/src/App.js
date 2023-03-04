d5d9a4168fe98ba0b2ccf3e44287915961454a74
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-03 17:04:49 -0700

    going with a different structure, previous idea was too complicated and didn't match the example

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 // First we import our Hello component from our components folder.
-import Hello from './components/Hello';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import HeroSection from "./HeroSection";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Resume from "./Resume";
 
 // React apps typically have a single App component at the very top that can reference other React components.
 // This component, `App`, is our main component that is importing `Hello` and rendering it in the return method.
-function App() {
-  return <Hello />;
+import React from "react";
+import HeroSection from "./PrimaryNavBar";
+
+function PrimaryNavBar() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ react-portfolio/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>Some information about me.</p>
+    </div>
+  );
+}
+
+export default About;
